6327e4ff80bb5a088df133572358aa51f210173b
Arturo Laurenzi <arturo.laurenzi@iit.it>
2020-06-07 21:12:00 +0200

    adding linux distro to deb file name

--- cmake/generate_deb_pkg.cmake
+++ cmake/generate_deb_pkg.cmake
@@ -7,6 +7,32 @@ set(CPACK_PACKAGE_ARCHITECTURE "amd64")
 set(CPACK_DEBIAN_PACKAGE_DEPENDS "")
 set(CPACK_DEBIAN_PACKAGE_MAINTAINER "arturo.laurenzi@iit.it")
 set(CPACK_DEBIAN_PACKAGE_DESCRIPTION "C++ library for logging numeric data to MAT-file")
-include(CPack)
 
-set(DEB_FILE_NAME ${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}-Linux.deb)
+find_program(GIT_SCM git DOC "Git version control")
+mark_as_advanced(GIT_SCM)
+find_file(GITDIR NAMES .git PATHS ${CMAKE_SOURCE_DIR} NO_DEFAULT_PATH)
+if (GIT_SCM AND GITDIR)
+    execute_process(
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        COMMAND ${GIT_SCM} log -1 "--pretty=format:%h"
+        OUTPUT_VARIABLE GIT_SHA1_SHORT
+    )
+else()
+    # No version control
+    # e.g. when the software is built from a source tarball
+    # and gitrevision.hh is packaged with it but no Git is available
+    message(STATUS "Will not remake ${SRCDIR}/gitrevision.hh")
+endif()
+
+execute_process(
+    COMMAND lsb_release -cs
+    OUTPUT_VARIABLE LINUX_DISTRO_NAME
+)
+
+string(REPLACE "\n" " " LINUX_DISTRO_NAME ${LINUX_DISTRO_NAME})
+
+set(CPACK_PACKAGE_FILE_NAME ${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}-${GIT_SHA1_SHORT}-${CPACK_PACKAGE_ARCHITECTURE}-${LINUX_DISTRO_NAME})
+
+message(STATUS "Will generate package '${CPACK_PACKAGE_FILE_NAME}'.deb")
+
+include(CPack)
