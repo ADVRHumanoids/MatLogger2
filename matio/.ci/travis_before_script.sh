225d0fc18b470e8b2cbc8ec18d56adfda08c0e06
andrea patrizi <andrepatrizi1b6e6@gmail.com>
2022-03-22 16:18:37 +0100

    added matio library to repo

---
+++ matio/.ci/travis_before_script.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -x #echo on
+
+if [[ "${USE_CMAKE:-no}" == "no" ]]; then
+    ./autogen.sh
+    if [[ "$HDF5_VERSION" == "foo" ]]; then
+        ./configure --quiet --enable-shared --enable-debug --enable-mat73=$ENABLE_MAT73 --enable-extended-sparse=$ENABLE_EXTENDED_SPARSE --with-zlib=$WITH_ZLIB --with-pic; CC=foo
+    fi
+    if [[ "$CC" == "gcc-4.8" ]]; then
+        ./configure --quiet --enable-shared --enable-coverage --enable-debug --enable-mat73=$ENABLE_MAT73 --enable-extended-sparse=$ENABLE_EXTENDED_SPARSE --with-zlib=$WITH_ZLIB --with-pic --with-hdf5=$TRAVIS_BUILD_DIR/hdf5-$HDF5_VERSION/hdf5
+    fi
+    if [[ "$CC" == "clang" ]]; then
+        ./configure --quiet --enable-shared --enable-debug --enable-mat73=$ENABLE_MAT73 --enable-extended-sparse=$ENABLE_EXTENDED_SPARSE --with-zlib=$WITH_ZLIB --with-pic --with-hdf5=$TRAVIS_BUILD_DIR/hdf5-$HDF5_VERSION/hdf5
+    fi
+    if [[ "$HDF5_VERSION" == "foo" ]]; then
+        CC=$(which clang)
+    fi
+fi
+if [[ "${USE_CMAKE:-no}" == "yes" ]]; then
+    if [[ "$TRAVIS_OS_NAME" == "linux" ]] && [[ "${USE_CONAN:-no}" == "no" ]]; then
+        HDF5_DIR=$HOME/CMake-hdf5-$HDF5_VERSION/HDF5-$HDF5_VERSION-Linux/HDF_Group/HDF5/$HDF5_VERSION
+        CMAKE_PREFIX_PATH=-DCMAKE_PREFIX_PATH=$HDF5_DIR/share/cmake
+    fi
+
+    if [[ "${USE_CONAN:-no}" == "yes" ]]; then
+        CMAKE_CONAN_ARG=-DMATIO_USE_CONAN=TRUE
+    fi
+
+    SRC_DIR=$TRAVIS_BUILD_DIR
+    BUILD_DIR=$HOME/matio_cmake
+    mkdir -p $BUILD_DIR
+    pushd $BUILD_DIR
+
+    cmake $SRC_DIR -DCMAKE_BUILD_TYPE=Debug \
+                   $CMAKE_PREFIX_PATH \
+                   -DMATIO_EXTENDED_SPARSE=$ENABLE_EXTENDED_SPARSE \
+                   -DMATIO_MAT73=$ENABLE_MAT73 \
+                   -DMATIO_WITH_HDF5=$ENABLE_MAT73 \
+                   -DMATIO_WITH_ZLIB=$WITH_ZLIB \
+                   -DMATIO_SHARED=TRUE \
+                   $CMAKE_CONAN_ARG
+    popd
+fi
