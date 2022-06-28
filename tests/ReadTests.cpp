f5036d94c9b021b75257235cf4c3360a32bb2755
andrea patrizi <andrepatrizi1b6e6@gmail.com>
2022-06-28 13:31:04 +0200

    code restyling and added assertions for matrix variable reading

--- tests/ReadTests.cpp
+++ tests/ReadTests.cpp
@@ -29,23 +29,99 @@ namespace
 }
 
 class ReadTests: public ::testing::Test {
-    
 
 protected:
 
-     ReadTests(){
-         
-     }
+    int n_rows_mat1 = 23, n_cols_mat1 = 14, n_slices_mat1 = 1, n_slices_block_mat = 5; // just some variables useful to check the correctness of reading operations
+    int n_rows_mat2 = 11, n_cols_mat2 = 34, n_slices_mat2 = 1; 
 
-     virtual ~ReadTests() {
-     }
+    std::string mat_path = "/tmp/readExample.mat";
+    std::string matrix_name = std::string("matrix"), matrix2_name = std::string("matrix2"),
+                matrix_tb_deleted_name = std::string("matrix_tb_deleted"), block_matrix_name = std::string("block_matrix"), 
+                cell_data_name = std::string("cell_data_name"), struct_data_name = std::string("struct_var"); 
+    
+    Eigen::MatrixXd matrix, matrix2, matrix_tb_deleted, block_matrix_check;
 
-     virtual void SetUp() {
-         
-     }
+    XBot::matlogger2::MatData struct_data;
+    XBot::matlogger2::MatData cell_data;
+    
+    ReadTests(){
+        
+        // first matrix
+        matrix = Eigen::MatrixXd(this->n_rows_mat1, this->n_cols_mat1 * this->n_slices_mat1);
+        int count = 0;
+        for (int i = 0; i < this->n_slices_mat1; i++) // initialize matrix with incrementing values, starting from 1
+        {
+            for (int j = 0; j < this->n_rows_mat1; j++)
+            {
+                for (int k = 0; k < this->n_cols_mat1; k++)
+                {
+                    count++;
+                    matrix(j, k + i * this->n_cols_mat1) = count;
+                }
+            }
+        }
 
-     virtual void TearDown() {
-     }
+        // second matrix (to test adding variables after loading mat file)
+        matrix2 = Eigen::MatrixXd(this->n_rows_mat2, this->n_cols_mat2 * this->n_slices_mat2);
+        int count2 = 0;
+        for (int i = 0; i < this->n_slices_mat2; i++) // initialize matrix with incrementing values, starting from 1
+        {
+            for (int j = 0; j < this->n_rows_mat2; j++)
+            {
+                for (int k = 0; k < this->n_cols_mat2; k++)
+                {
+                    count2--;
+                    matrix2(j, k + i * this->n_cols_mat2 ) = count2;
+                }
+            }
+        }
+
+        // auxiliary block matrix used to check the correct writing and reading of block matrix
+
+        block_matrix_check = Eigen::MatrixXd(this->n_rows_mat1, this->n_cols_mat1 * this->n_slices_block_mat);
+        for (int i = 0; i < this->n_slices_block_mat; i++) // initialize matrix with incrementing values, starting from 1
+        {
+            for (int j = 0; j < this->n_rows_mat1; j++)
+            {
+                for (int k = 0; k < this->n_cols_mat1; k++)
+                {
+                    block_matrix_check(j, k + i * this->n_cols_mat1 ) = matrix(j, k);
+                }
+            }
+        }
+
+        // structure
+        struct_data = XBot::matlogger2::MatData::make_struct();
+        struct_data["field_1"] = XBot::matlogger2::MatData::make_cell(3);
+        struct_data["field_2"] = Eigen::MatrixXd::Identity(5, 8);
+        struct_data["field_3"] = XBot::matlogger2::MatData::make_struct();
+        struct_data["field_1"].asCell() = {1.0,
+                "ci\nao",
+                Eigen::MatrixXd::Identity(2,5)};
+        struct_data["field_3"]["subfield_1"] = 1;
+        struct_data["field_3"]["subfield_2"] = 2.4;
+        struct_data["field_3"]["subfield_3"] = 3.1;
+        struct_data["field_3"]["subfield_4"] = 4.9;
+
+        // cell
+        int cell_size = 3;
+        cell_data = XBot::matlogger2::MatData::make_cell(cell_size);
+        cell_data[0] = Eigen::Vector2d::Random();
+        cell_data[1] = Eigen::Vector3d::Random();
+        cell_data[2] = Eigen::Vector4d::Random();
+
+    }
+
+    virtual ~ReadTests() {
+    }
+
+    virtual void SetUp() {
+        
+    }
+
+    virtual void TearDown() {
+    }
      
      
 };
@@ -53,61 +129,18 @@ protected:
 TEST_F(ReadTests, dump_sample_file)
 {
 
+    remove(this->mat_path.c_str()); // remove test file if already existing
+
     using namespace XBot::matlogger2;
 
-    int n_rows1 = 23, n_cols1 = 14, n_slices1 = 1;
-    
-    Eigen::MatrixXd matrix(n_rows1, n_cols1 * n_slices1);
-    int count = 0;
-    for (int i = 0; i < n_slices1; i++) // initialize matrix with incrementing values, starting from 1
+    auto logger = XBot::MatLogger2::MakeLogger(this->mat_path);
+    ASSERT_TRUE(logger->save(this->struct_data_name, this->struct_data));
+    ASSERT_TRUE(logger->save(this->cell_data_name, this->cell_data));
+    ASSERT_TRUE(logger->add(matrix_name, this->matrix));
+
+    for (int i = 0; i < this->n_slices_block_mat; i++)
     {
-        for (int j = 0; j < n_rows1; j++)
-        {
-            for (int k = 0; k < n_cols1; k++)
-            {
-                count++;
-                matrix(j, k + i * n_cols1 ) = count;
-            }
-        }
-    }
-
-    auto struct_data = MatData::make_struct();
-    struct_data["field_1"] = MatData::make_cell(3);
-    struct_data["field_2"] = Eigen::MatrixXd::Identity(5, 8);
-    struct_data["field_3"] = MatData::make_struct();
-
-    struct_data["field_1"].asCell() = {1.0,
-            "ci\nao",
-            Eigen::MatrixXd::Identity(2,5)};
-
-    struct_data["field_3"]["subfield_1"] = 1;
-    struct_data["field_3"]["subfield_2"] = 2.0;
-    struct_data["field_3"]["subfield_3"] = 3.0;
-    struct_data["field_3"]["subfield_4"] = 4.0;
-
-    auto struct_data_cpy = struct_data;
-
-    struct_data_cpy["field_2"].value().as<Eigen::MatrixXd>() = Eigen::Matrix3d::Random();
-
-    int cell_size = 3;
-    auto cell_data = MatData::make_cell(cell_size);
-    cell_data[0] = Eigen::Vector2d::Random();
-    cell_data[1] = Eigen::Vector3d::Random();
-    cell_data[2] = Eigen::Vector4d::Random();
-
-    std::string mat_path = "/tmp/readExample.mat";
-
-    // XBot::MatLogger2::Options opts;
-    // opts.load_file_from_path = false;
-
-    auto logger = XBot::MatLogger2::MakeLogger(mat_path);
-    logger->save("mvar", struct_data);
-    logger->save("cellvar", cell_data);
-    logger->add("matrix", matrix);
-
-    for (int i = 0; i < 5; i++)
-    {
-        logger->add("block_matrix", matrix);
+        ASSERT_TRUE(logger->add("block_matrix", matrix));
     } 
     logger.reset();
 
@@ -118,28 +151,10 @@ TEST_F(ReadTests, load_and_modify)
     XBot::MatLogger2::Options opts;
     opts.load_file_from_path = true;
 
-    std::string mat_path = "/tmp/readExample.mat";
+    auto logger = XBot::MatLogger2::MakeLogger(this->mat_path, opts);
 
-    auto logger = XBot::MatLogger2::MakeLogger(mat_path, opts);
-
-    int n_rows1 = 11, n_cols1 = 34, n_slices1 = 1;
-    
-    Eigen::MatrixXd matrix(n_rows1, n_cols1 * n_slices1);
-    int count = 0;
-    for (int i = 0; i < n_slices1; i++) // initialize matrix with incrementing values, starting from 1
-    {
-        for (int j = 0; j < n_rows1; j++)
-        {
-            for (int k = 0; k < n_cols1; k++)
-            {
-                count--;
-                matrix(j, k + i * n_cols1 ) = count;
-            }
-        }
-    }
-
-    logger->add("new_matrix", matrix);
-    logger->add("new_matrix_copy", matrix);
+    ASSERT_TRUE(logger->add(this->matrix2_name, this->matrix2));
+    ASSERT_TRUE(logger->add(this->matrix_tb_deleted_name, this->matrix2));
 
     logger.reset();
 
@@ -180,13 +195,13 @@ TEST_F(ReadTests, delete_var)
 
     auto logger = XBot::MatLogger2::MakeLogger(mat_path, opts);
 
-    ASSERT_TRUE(logger->delvar("new_matrix_copy"));
+    ASSERT_TRUE(logger->delvar(this->matrix_tb_deleted_name));
 
     logger.reset();
 
 }
 
-TEST_F(ReadTests, read_var_names2)
+TEST_F(ReadTests, check_var_deleted_ok)
 {
     XBot::MatLogger2::Options opts;
     opts.load_file_from_path = true;
@@ -204,12 +219,20 @@ TEST_F(ReadTests, read_var_names2)
     logger.reset();
 
     int j = 0;
+    bool was_var_del_ok = true;
     for (int j = 0; j < n_vars; ++j) // printing info on all variables
     {   
 
-        std::cout << "Variable: " << var_names[j] << "\n"; 
+        std::cout << "Variable: " << var_names[j] << "\n";
+        
+        if (var_names[j] == this->matrix_tb_deleted_name)
+        {
+            was_var_del_ok = false;
+        }
                 
     }
+
+    ASSERT_TRUE(was_var_del_ok);
 }
 
 TEST_F(ReadTests, read_and_print)
@@ -221,27 +244,38 @@ TEST_F(ReadTests, read_and_print)
 
     auto logger = XBot::MatLogger2::MakeLogger(mat_path, opts);
 
-    Eigen::MatrixXd read_matrix;
+    Eigen::MatrixXd matrix, matrix2, block_matrix;
     int slices;
 
-    ASSERT_TRUE(logger->readvar("new_matrix", read_matrix, slices));
-    
-    std::cout << "read matrix: " << read_matrix << std::endl;
+    ASSERT_TRUE(logger->readvar(matrix_name, matrix, slices));
+    std::cout << "\n" << "read " << this->matrix_name.c_str() << ": "<< "\n" << matrix << std::endl;
     std::cout << "n. slices: " << slices << std::endl;
-    
-    ASSERT_TRUE(logger->readvar("block_matrix", read_matrix, slices));
-    
-    std::cout << "read block matrix: " << read_matrix << std::endl;
+    ASSERT_TRUE(matrix.rows() == this->n_rows_mat1);
+    ASSERT_TRUE(matrix.cols() == this->n_cols_mat1 * this->n_slices_mat1);
+    ASSERT_TRUE(matrix.isApprox(this->matrix));
+
+    ASSERT_TRUE(logger->readvar(this->matrix2_name, matrix2, slices));
+    std::cout << "\n" << "read " << this->matrix2_name.c_str() << ": " << "\n" << matrix2 << std::endl;
     std::cout << "n. slices: " << slices << std::endl;
+    ASSERT_TRUE(matrix2.rows() == this->n_rows_mat2);
+    ASSERT_TRUE(matrix2.cols() == this->n_cols_mat2 * this->n_slices_mat2);
+    ASSERT_TRUE(matrix2.isApprox(this->matrix2));
     
-    XBot::matlogger2::MatData matdata;
-    ASSERT_TRUE(logger->read_container("cellvar", matdata));
+    ASSERT_TRUE(logger->readvar("block_matrix", block_matrix, slices));
+    std::cout << "\n" << "read " << this->block_matrix_name.c_str() << ": " << "\n"  << block_matrix << std::endl;
+    std::cout << "n. slices: " << slices << std::endl;
+    ASSERT_TRUE(block_matrix.rows() == this->n_rows_mat1);
+    ASSERT_TRUE(block_matrix.cols() == this->n_cols_mat1 * this->n_slices_block_mat);
+    ASSERT_TRUE(block_matrix.isApprox(this->block_matrix_check));
 
-    matdata.print();
+    XBot::matlogger2::MatData struct_data, cell_data;
+    ASSERT_TRUE(logger->read_container(this->cell_data_name, struct_data));
+    std::cout << "\n" << "read " << this->cell_data_name.c_str() << ": " << std::endl;
+    struct_data.print();
 
-    ASSERT_TRUE(logger->read_container("mvar", matdata));
-
-    matdata.print();
+    ASSERT_TRUE(logger->read_container(this->struct_data_name, cell_data));
+    std::cout << "\n" << "read structure: " << std::endl;
+    cell_data.print();
 
     logger.reset();
 
