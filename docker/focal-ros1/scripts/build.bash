f60ce32d70f439b5c6d3062842cae51f66049366
Arturo Laurenzi <arturo.laurenzi@iit.it>
2025-08-25 14:52:12 +0200

    port ci to gh

---
+++ docker/focal-ros1/scripts/build.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+# make forest https
+export HHCM_FOREST_CLONE_DEFAULT_PROTO=https
+
+# create ws and source it
+mkdir -p test_ws && cd test_ws
+forest init
+
+# setup env
+source /opt/ros/noetic/setup.bash
+source setup.bash
+
+# add recipes
+forest add-recipes git@github.com:advrhumanoids/multidof_recipes.git -t master
+
+# build
+export PYTHONUNBUFFERED=1
+forest grow pybind11 --verbose --clone-depth 1 -j ${FOREST_JOBS:-1}
+forest grow matlogger2 --verbose --clone-depth 1 -j ${FOREST_JOBS:-1}
+
+# build tests
+cd build/matlogger2
+cmake -DBUILD_TESTS=1 .
+make -j ${FOREST_JOBS:-1}
---
+++ docker/focal-ros1/scripts/test.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+
+cd test_ws/build/matlogger2
+ctest --output-on-failure
